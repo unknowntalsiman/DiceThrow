f93a24126ffaace2016eaf0f6533e264e0a8fa99
unknowntalsiman <tuo98558@temple.edu>
2025-03-18 12:11:21 -0400

    state information was not retained when device was turned. Implemented a method to maintain the previous roll when I rotate my phone, so basically the fragment is not destroyed and rebuilt like it was previously.

--- app/src/main/java/edu/temple/dicethrow/DieFragment.kt
+++ app/src/main/java/edu/temple/dicethrow/DieFragment.kt
@@ -16,6 +16,10 @@ class DieFragment : Fragment() {
 
     var dieSides: Int = 6
 
+    companion object {
+        const val CURRENT_ROLL = "current_roll"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -37,11 +41,21 @@ class DieFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        throwDie()
-        view.setOnClickListener{
+        if (savedInstanceState != null) {
+            dieTextView.text = savedInstanceState.getString(CURRENT_ROLL)
+        } else {
+            throwDie()
+        }
+        view.setOnClickListener {
             throwDie()
         }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(CURRENT_ROLL, dieTextView.text.toString())
+    }
+
 
     fun throwDie() {
         dieTextView.text = (Random.nextInt(dieSides) + 1).toString()
